4445c36a4aa37fb17234d9314a598ea244cd67fd
lukmanxpert <lukmanmiah2004@gmail.com>
2026-05-06 22:24:58 +0600

    feat(global): added env variables

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
+import { envVariables } from "../../config/env";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = envVariables.DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
