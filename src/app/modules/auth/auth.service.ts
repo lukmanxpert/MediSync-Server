c51e557575b384d3c2f940f06ef8d373a4ad0fba
lukmanxpert <lukmanmiah2004@gmail.com>
2026-05-06 17:50:47 +0600

    feat(auth): added register api

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { auth } from "../../lib/auth";
+interface IRegisterPatientPayload {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const registerPatient = async (payload: IRegisterPatientPayload) => {
+  const { name, email, password } = payload;
+  const data = await auth.api.signUpEmail({
+    body: {
+      name,
+      email,
+      password,
+    },
+  });
+  if (!data.user) {
+    throw new Error("Failed to register patient");
+  }
+
+  //   todo: Create patient profile In transaction after sign up of patient in User model
+  //   const patient = await prisma.$transaction(async (prisma) => {
+  //     const user = await prisma.user.update({
+  //       where: {
+
+  return data;
+};
+
+export const AuthService = {
+  registerPatient,
+};
