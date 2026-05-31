f9c91fe20190ed11fdad44092d11fbf843812c82
lukmanxpert <lukmanmiah2004@gmail.com>
2026-06-01 01:34:01 +0600

    feat(Cloudinary): added file upload and delete fn

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -3,6 +3,10 @@ import * as z from "zod";
 
 export const validateRequest = (schema: z.ZodObject) => {
   return (req: Request, res: Response, next: NextFunction) => {
+    if (req.body.data) {
+      req.body = JSON.parse(req.body.data);
+    }
+
     const parsedData = schema.safeParse(req.body);
     if (!parsedData.success) {
       next(parsedData.error);
